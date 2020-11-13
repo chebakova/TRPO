f3b1968a5ce9938d4453fe02736880df3ca47270
git <git@MacBook-Pro-Vladislav.local>
2020-11-13 12:06:02 +0300

    3lab

---
+++ chebakova/Line.php
@@ -0,0 +1,16 @@
+<?php
+
+Class Line
+{
+    public function solve($a, $b)
+    {
+        if ($a == 0) {
+            return NULL;
+        }
+        return $this->X = -($b / $a);
+    }
+
+    protected $X;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,38 +1,18 @@
-<?php 
-Class A {
-	public function equation ($a,$b) {
-		if ($a == 0){
-		  return false;
-		}
-		return $this->X=-($b/$a);
-	}
-	protected $X;
-}
-Class B extends A {
-    protected function diskriminant($a, $b, $c) {
-		$x = ($b**2)-4*$a*$c;
-		return $x;
-	}
-	public function qu_equation($a, $b, $c){
-		$d = $this->diskriminant($a,$b,$c);
-		if($a == 0){
-			return $this->equation($b,$c);
-		}
-		if ($d > 0) {
-			return $this->X=array (
-				-($b+sqrt($d)/(2*$a)),
-				-($b-sqrt($d)/(2*$a))
-			);
-		}
-		
-		if ($d == 0) {
-			return $this->X=array(-($b/(2*$a)));
-		}
-		return $this->X=false;
-	}
-}
-	$a = new A();
-	$b = new B();
-	
-	?>
-			
+<?php
+use chebakova\MyLog; 
+
+include './core/EquationInterface.php'; 
+include './core/LogAbstract.php'; 
+include './core/LogInterface.php'; 
+include './chebakova/Line.php'; 
+include './chebakova/Sqr.php'; 
+include './chebakova/MyLog.php'; 
+
+$a = MyLog::Instance(); 
+$b = MyLog::Instance(); 
+
+$a->log('1'); 
+$a->log('2'); 
+$a->log('3'); 
+$a->log('4'); 
+$b->write();
